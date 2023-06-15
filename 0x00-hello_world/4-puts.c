4ce6367b9ed1acee54b0b66fafb6a076c7a22336
IyanuoluwaAkinwale <ashjaley@gmail.com>
2023-06-15 16:11:58 -0700

    puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <std.io>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle"\n);
+	puts("\"Programming is like building a multilingual puzzle"\n);
 	return (0);
 }
