c81cb276d7a05555ff9438d897031942da2a5a0b
IyanuoluwaAkinwale <ashjaley@gmail.com>
2023-06-21 10:19:14 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabet in lowercase,
+ *	and then in uppercase,then a new line.
+ * alp - character
+ *
+ * Return - Always
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+	for (alp = 'A'; alp <= 'Z'; alp++)
+		putchar(alp);
+	putchar('\n');
+
+	return (0);
+}
