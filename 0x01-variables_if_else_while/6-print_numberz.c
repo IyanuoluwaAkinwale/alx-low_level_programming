462d934616ab51fb960bca3628582a8aa5e52579
IyanuoluwaAkinwale <ashjaley@gmail.com>
2023-06-21 11:41:15 -0700

    base 10[

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int b = 0;
+
+	while (b < 10)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+
+	return (0);
+}
