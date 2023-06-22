23ee9db11ff37efa22f5bb88dbb48ea28bc9912f
IyanuoluwaAkinwale <ashjaley@gmail.com>
2023-06-22 13:15:12 -0700

    nested loop to print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+
+void print_alphabet_x10(void)
+{
+	int count;
+	char alp;
+
+	for (count = 1; count <= 9; count++)
+	{
+		for (alp = 'a'; alp <= 'z'; alp++)
+			_putchar(alp);
+		_putchar('\n');
+	}
+}
