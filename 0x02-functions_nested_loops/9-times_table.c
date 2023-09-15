020afe132af8665cf1e6dccce57793e74a0e3fa7
IyanuoluwaAkinwale <ashjaley@gmail.com>
2023-09-15 13:05:11 +0100

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,26 +10,33 @@
 
 void times_table(void)
 {
-	int row, col, result;
+	int row, column, product, tens, ones;
 
-	for (row = 0; col <= 9; row++)
+	for (row = 0; row <= 9; row++)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (col = 1; col <= 9; col++)
+		for (column = 0; column <= 9; column++)
 		{
-			result = (row * col);
-			if ((result / 10) > 0)
-				_putchar((result / 10) + '0');
-			else
-				_putchar(' ');
-			_putchar((result % 10) + '0');
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
 
-			if (col < 9)
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
 			}
 		}
 		_putchar('\n');
