c7c8c71b94763cfa21495cfd21a87a8ecbcc1dc6
TianaAkinwale <tianorway@gmail.com>
2023-09-27 14:26:27 +0100

    testing

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,4 +9,16 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int count = 0;
 
+	while (*s != '\0' && *accept != '\0')
+	{
+		if (*s == *accept)
+		{
+			count++;
+		}
+		s++;
+		accept++;
+	}
+	return (count);
+}
