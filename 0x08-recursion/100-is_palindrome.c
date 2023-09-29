db54f4b44a4335d1adb3d2af9a151c4d5cfe6d8e
TianaAkinwale <tianorway@gmail.com>
2023-09-29 11:28:21 +0100

    oh well

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,7 +4,7 @@ int find_strlen(char *s);
 int check_palindrome(char *s, int i);
 
 /**
- * strlen_recursion - Returns the length of a string.
+ * _strlen_recursion - Returns the length of a string.
  * @s: The string to be measured.
  *
  * Return: The length of the string.
@@ -25,21 +25,20 @@ int _strlen_recursion(char *s)
 /**
  * check_palindrome - Checks if a string is a palindrome.
  * @s: The string to be checked
- * @i: The index of the string to be checked.
+ * @length: length of string to be checked
+ * @index: The index of the string to be checked.
  *
  * Return: If the string is a palindrome - 1.
  *         If the string is not a palindrome - 0.
  */
 
-int check_palindrome(char *s, int i)
+int check_palindrome(char *s, int length, int index)
 {
-	int length = _strlen_recursion(s);
-
-       /* if (s[i] == s[length / 2])*/
+	/* if (s[i] == s[length / 2])*/
 	/* return (1);*/
 
-	if (s[i] == s[length - 1 - i])
-		return (check_palindrome(s + 1, i + 1));
+	if (s[index] == s[length - 1 - index])
+		return (check_palindrome(s + 1, index + 1));
 
 	return (0);
 }
@@ -54,10 +53,10 @@ int check_palindrome(char *s, int i)
 int is_palindrome(char *s)
 {
 	int length = _strlen_recursion(s);
-	int i = 0;
+	int index = 0;
 
 	if (length <= 1)
 		return (1);
 
-	return (check_palindrome(s, i));
+	return (check_palindrome(s, length, index));
 }
