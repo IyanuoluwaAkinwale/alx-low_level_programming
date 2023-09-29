88f0e857341be8e2380dafeb97fcc3fb96c0f0a5
TianaAkinwale <tianorway@gmail.com>
2023-09-29 11:08:34 +0100

    fix argument

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,6 +13,43 @@ int _strlen_recursion(char *s)
 }
 
 /**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
+}
+
+int check_palindrome(char *s, int i)
+{
+	int length = _strlen_recursion(s);
+
+	if (length <= 1)
+                return (1);
+
+        /*if (s[i] == s[length / 2])
+                return (1);*/
+
+        if (s[i] == s[length - 1 - i])
+                return (is_palindrome(s + 1, i + 1));
+
+	return 0;
+}
+
+/**
  * is_palindrome - Checks if a string is a palindrome.
  * @s: The string to be checked.
  *
@@ -28,11 +65,5 @@ int is_palindrome(char *s)
 	if (length <= 1)
 		return (1);
 
-	/*if (s[i] == s[length / 2])
-		return (1);*/
-
-	if (s[i] == s[length - 1 - i])
-		return (is_palindrome(s + 1));
-
-	return 0;
+	return (check_palindrome(s, i);
 }
