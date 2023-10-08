41b8e48e6f915d369d12c88e9f1a50202f23df54
TianaAkinwale <tianorway@gmail.com>
2023-10-08 11:17:52 +0100

    shjs

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Find all .c files in the current directory and compile them into object files
 gcc -c *.c
-
-# Create a static library from the object files
 ar rcs liball.a *.o
-
-# Clean up the object files
 rm -f *.o
-
