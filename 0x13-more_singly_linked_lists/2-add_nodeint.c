d747076a6153f5bf841ee79b85f086fb94d91aa9
IyanuoluwaAkinwale <ashjaley@gmail.com>
2023-10-24 14:08:30 +0100

    new

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-[I	new = malloc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
 		return (NULL);
